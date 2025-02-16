0bad9749506ebabbfb3eff53e251655a618fb5ad
Mayst <shermainewang.night@gmail.com>
2025-02-16 04:53:10 -0800

    Loading data and Char lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
+import ClientsData from "./data/clients.json"
 
 export default function Home() {
+
+  console.log(ClientsData);
+
+  ClientsData.map((client) => {console.log(client)});
+
+  // Extract sales from clients
+  const salesData = ClientsData.flatMap(product => product.sales);
+  console.log(salesData);
+
+  // Setting headers for table
+  const headers = ["WEEK ENDING", "RETAIL SALES", "WHOLESALE SALES", "UNITS SOLD", "RETAILER MARGIN"]
+
+
   return (
     <main className="flex flex-col justify-between pt-5 pl-5">
      {/* Header */}
@@ -13,7 +27,7 @@ export default function Home() {
       {/* Body */}
       <section className="flex justify-between">
         <div>
-          <h1 className="text-3xl lg:text-5xl font-extrabold mb-6">Advertisement</h1>
+          <h1 className="text-3xl lg:text-2xl mb-6">Advertisement</h1>
           <div className="flex justify-start gap-2">
              <div className="opacity-90 mb-10">
               select_1
@@ -28,9 +42,21 @@ export default function Home() {
         </div>
      
       {/* Dashboard */}
-        <div className="opacity-90 mb-10 flex-col gap-6">
-          <div> Graph </div>
-          <div> Table </div>
+        <div className="opacity-90 flex-col gap-6">
+          <div> 
+            <ul className="max-w-lg mx-auto">
+              {/* {sales.map(sale) => 
+                <li key=""></li>
+              )} */}
+            </ul> 
+          </div>
+          <div> 
+            <ul className="max-w-lg mx-auto">
+              {/* {sales.map((sale) => 
+                <li key={sale.title}></li>
+              )} */}
+            </ul> 
+          </div>
         </div>
       
       </section>
